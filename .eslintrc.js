ceedb9d644db5b3a1f7fc4d6f18aff3fdc25860e
Nick Mispoulier <nick@proclame.be>
2025-03-18 13:37:23 +0100

    comment eslint noconsole rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,26 @@ module.exports = {
   env: {
     node: true,
     es2021: true,
-    browser: true
+    browser: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   ignorePatterns: ['dist/**', 'node_modules/**', 'build/**', '.eslintrc.js'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   plugins: ['react'],
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
   rules: {
     'no-unused-vars': 'warn',
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'react/prop-types': 'off'
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'react/prop-types': 'off',
   },
 };
